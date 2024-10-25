5b374987d55815037a6ceef00710e899e1a429ee
advith <devops369@gmail.com>
2024-10-25 16:50:29 +0000

    create deployment script

---
+++ script.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#echo "updating system"
+#sudo apt update -y
+#
+#echo "installing utilities"
+#sudo apt install -y zip unzip
+#
+#echo installing "NGINX web server"
+#sudo apt install -y nginx
+#
+#echo "remove samples pages"
+#sudo rm -rf /var/www/html/
+#
+#echo "clone login app"
+#sudo git clone https://github.com/pendurtisanjeev/login-2426.git
+#
+#echo "script excution completed"
+
